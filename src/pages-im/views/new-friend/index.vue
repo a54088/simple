19fe009bf01dddec85734c78e5d2b3b0b3c6b279
wangshuo <wangshuo_9527@163.com>
2025-11-11 11:04:51 +0800

    nm

--- src/pages-im/views/new-friend/index.vue
+++ src/pages-im/views/new-friend/index.vue
@@ -1,6 +1,18 @@
-<script setup></script>
+<script setup>
+import CustomNavbar from '@/components/custom-navbar/index.vue'
+</script>
 
 <template>
-new-friend
+    <view class="new-friend__layout">
+        <CustomNavbar title="新的朋友" bgColor="transparent" />
+        <view class="new-friend__content">
+            
+        </view>
+    </view>
 </template>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.new-friend__content {
+    padding: 54rpx 32rpx;
+
+}
+</style>
